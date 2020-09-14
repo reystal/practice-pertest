32378ca457940efe0fcfc21f9bf3d99f78206675
reystal <51355248+reystal@users.noreply.github.com>
2020-09-14 17:41:48 +0800

    求给定二叉树的最大深度
    思路:递归的方法
    二叉树的最大深度=根节点（1）+max（左子树的深度，右子树的深度）
    直到root==null结束
    1.root==null，返回0；
    2.左子树的深度
    3.右子树的深度
    4.最大深度=1+max（左子树最大深度，右子树最大深度)

---
+++ test-9-14-二叉树/maxDepth.java
@@ -0,0 +1,28 @@
+ import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ * }
+ */
+
+public class Solution {
+    /**
+     * 
+     * @param root TreeNode类 
+     * @return int整型
+     */
+    public int maxDepth (TreeNode root) 
+    {
+        // write code here
+        if(root==null)
+        {
+            return 0;
+        }
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
